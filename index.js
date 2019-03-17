ca5bab4103633595080e25483124b54f5e835568
Jessica <JdenHollander@users.noreply.github.com>
2019-03-17 12:17:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+alert("Reload this page");
+
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var image1 = "images/dice"+ randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src",image1);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var image2 = "images/dice"+ randomNumber2 + ".png";
+var newimage2 = document.querySelectorAll("img")[1];
+newimage2.setAttribute("src",image2);
+
+if(randomNumber1 > randomNumber2){
+document.querySelector("h1").innerHTML = "🎈Player 1 Wins!"
+}
+else if(randomNumber1 < randomNumber2){
+document.querySelector("h1").innerHTML = "Player 2 Wins🎈"
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw!"
+  }
